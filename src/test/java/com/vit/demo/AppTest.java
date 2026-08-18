ea2b59f04cff4afa7f6c2c253d1538ba061e3a38
Sneha17 <snehag1795@gmail.com>
2026-08-18 23:30:25 +0530

    Introduce failing test for CI demonstration

--- src/test/java/com/vit/demo/AppTest.java
+++ src/test/java/com/vit/demo/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest {
     @Test
     public void testAdd() {
         int result = App.add(2, 3);
-        int expected = 5;
+        int expected = 6;
         assertEquals(expected, result);
         System.out.println("TEST PASSED: App.add(2, 3) returned " + result + ".");
     }
